dc646b2729e43be5c78f1b858fc7782621efa230
qingtian881223 <137573155@qq.com>
2017-12-29 11:04:50 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyEditor from  "./MyEditor";
+import SimpleInlineToolbarEditor from "./SimpleInlineToolbarEditor";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <MyEditor></MyEditor>
-      </div>
+        <SimpleInlineToolbarEditor />
     );
   }
 }
